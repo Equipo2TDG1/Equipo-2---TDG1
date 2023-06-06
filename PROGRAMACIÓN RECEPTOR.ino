17fcce1c873d8d14507cb5f7d45ce4a5d0d67568
Josema1304 <118083275+Josema1304@users.noreply.github.com>
2023-06-05 22:37:27 -0500

    Update PROGRAMACIÓN RECEPTOR.ino

--- PROGRAMACIÓN RECEPTOR.ino
+++ PROGRAMACIÓN RECEPTOR.ino
@@ -28,3 +28,49 @@ void loop() {
     Serial.println(analogData.level);
   }
 }
+
+// Este ejemplo recupera el último valor de una variable de la API de Ubidots                      
+// utilizando el protocolo TCP. 
+
+/**************************************** 
+* Incluir bibliotecas 
+****** *********************************/ 
+#incluir <Ubidots.h>   
+
+/******* ******************************** 
+* Definir constantes 
+************** **************************/ 
+
+// Sus credenciales GPRS, si las hay 
+const char *APN = "Pon_el_APN_aquí"; 
+const char *USUARIO = "Pon_el_usuario_APN_aquí"; 
+const char *PASS = "Pon_el_APN_pwd_aquí"; 
+const char *FICHA = "Pon_tu_ficha_de_Ubidots_aquí"; 
+const char *DEVICE_LABEL = "Pon_tu_dispositivo_API_etiqueta";
+const char *VARIABLE_LABEL = "Pon_tu_variable_API_etiqueta"; 
+
+Cliente de Ubidots (TOKEN, APN, USUARIO, PASS); 
+
+/**************************************** 
+* Funciones auxiliares 
+****** *********************************/ 
+                          
+// Pon aquí tus funciones auxiliares      
+
+/******* ******************************** 
+* Funciones principales 
+************** **************************/   
+
+void setup() { 
+  Serial.begin(115200); 
+  client.setDebug(true);// Establecer verdadero para que los mensajes de depuración estén disponibles 
+} 
+
+void loop() { 
+  float value = client.get(DEVICE_LABEL, VARIABLE_LABEL); 
+  if(value!=ERROR_VALUE){ 
+    Serial.print("Obteniendo el valor de la variable: "); 
+    Serial.println(valor);
+  } 
+  retraso (5000); 
+} 
